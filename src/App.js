fa899a7f8735468d8369f66c57c6757f60d5c594
truong.tt <truong.tt@sutrixmedia.com>
2016-07-27 18:29:42 +0700

    test Object.assign

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
+let obj = {
+  name: 'a',
+  age: 24
+}
+
 export default class App extends Component {
+  checkLog() {
+    return {
+      ...obj,
+      address: 'abc'
+    }
+    // return Object.assign({}, obj, {address: 'def'});
+  }
   render() {
+    let entry = this.checkLog();
+    console.log(entry, JSON.stringify(entry));
     return (
-      <h1>Hello, world.</h1>
+      <h1>Hello, world. {JSON.stringify(entry) + ' ; ' + Object.assign}</h1>
     );
   }
 }
